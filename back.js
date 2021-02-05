fa4061e8acd477063e82d373aeeb847aee67c953
Alfredo Rodriguez <alfredo.a.rodriguez02@utrgv.edu>
2021-02-05 01:29:45 -0600

    init

--- back.js
+++ back.js
@@ -1,17 +1,13 @@
 $(function(){
 
+    $('#searchClick').click(function(){
+        let usertext = $("#userbox").val();
+        console.log(usertext);
+        window.location.href = 'results.html' + '#' + usertext;
+     })
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(function(){
+    var usertext = window.location.hash.substring(1)
+    $(".card-title").text(usertext);
 });
