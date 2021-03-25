e613e13d5a09a0499f88a4b81264e0ae86bad72a
mflores0572 <61068473+mflores0572@users.noreply.github.com>
2021-03-25 14:50:37 -0500

    Add files via upload
    Server/Scraping Scripts

---
+++ WalmartServer.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+#from requests_html import HTMLSession
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+finlist = []
+sublist=[]
+
+def test(query):
+	print("Opening window....")
+	driver = webdriver.Chrome()
+
+	print("Opened window....")
+
+	URL = "https://www.walmart.com/search/?query=" + query
+	driver.get(URL)
+	print("Loading page....")
+
+	soup = BeautifulSoup(driver.page_source, 'html.parser')
+	print("Gathering listings....")
+
+	name = soup.findAll('a', attrs={'class': 'product-title-link line-clamp line-clamp-2 truncate-title'})
+	price = soup.findAll('span', attrs={'class': 'price display-inline-block arrange-fit price price-main'})
+	pics = soup.findAll('img', attrs={'data-pnodetype': 'item-pimg'})
+
+	#print(name)
+	#print(price)
+	print()
+	itrt = 0
+	"""for i in range(5):
+    		print(name[i].text)
+    		print(price[i].text)
+    		print(pics[i]['src'])
+    		finlist.append(name[i].text)
+    		finlist.append(price[i].text)
+    		finlist.append(pics[i]['src'])
+    		print()
+    		print()
+    	"""
+    	for (x,y,z) in (name,price,pics):
+    		sublist.append(pics[i]['src'])
+    		sublist.append(name[i].text)
+    		sublist.append(price[i].text)
+    		finlist.append(sublist)
+    		if(itrt>=5):
+    			break
+	
+	driver.quit()
+	return finlist
+#test("tv22")
---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/success/<name>')
+def success(name):
+   import bestbuyServer
+   import WalmartServer
+   temp = 'I THINK IT WORKS %s' % name
+   BB = str(bestbuyServer.test(name))
+   WM = str(WalmartServer.test(name))
+   return(BB+'\n'+WM)
+
+if __name__ == '__main__':
+   app.run(debug = True)
