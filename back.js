fd9c1dbef823003320a317de62058e3388284b60
mflores0572 <61068473+mflores0572@users.noreply.github.com>
2021-03-25 15:05:47 -0500

    Update back.js
    added python functionality

--- back.js
+++ back.js
@@ -10,4 +10,17 @@ $(function(){
 $(function(){
     var usertext = window.location.hash.substring(1)
     $(".card-title").text(usertext);
+    //console.log("HI")
+    function runPy() {
+    console.log("HI2")
+    $.ajax({
+    //type: "POST",
+    data : usertext,
+        url:'http://localhost:5000/success/'+usertext,
+        success: function(data) {                                                
+            console.log(data)
+        }
+    });
+}
+runPy()
 });
