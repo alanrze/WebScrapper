69c180dfedb2c0c7f7945e8573700d35026cb587
mflores0572 <61068473+mflores0572@users.noreply.github.com>
2021-04-01 23:42:00 -0500

    Update server.py
    currently focusing on Bestbuy

--- server.py
+++ server.py
@@ -7,11 +7,12 @@ CORS(app)
 @app.route('/success/<name>')
 def success(name):
    import bestbuyServer
-   import WalmartServer
+   """import WalmartServer
    temp = 'I THINK IT WORKS %s' % name
    BB = str(bestbuyServer.test(name))
    WM = str(WalmartServer.test(name))
-   return(BB+'\n'+WM)
+   return(BB+'\n'+WM)"""
+   return bestbuyServer.test(name)
 
 if __name__ == '__main__':
    app.run(debug = True)
