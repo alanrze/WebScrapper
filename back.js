7a8b78b3aff39834f08da4dbd278222d82dca3ef
Ronaldo Lopez <73009787+RLopez869@users.noreply.github.com>
2021-04-06 01:51:57 -0500

    Update back.js
    Added Walmart functionality

--- back.js
+++ back.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $('#searchClick').click(function(){
         let usertext = $("#userbox").val();
         console.log(usertext);
@@ -7,20 +6,71 @@ $(function(){
      })
 });
 
+//BestBuy
 $(function(){
     var usertext = window.location.hash.substring(1)
-    $(".card-title").text(usertext);
-    //console.log("HI")
-    function runPy() {
-    console.log("HI2")
-    $.ajax({
-    //type: "POST",
-    data : usertext,
-        url:'http://localhost:5000/success/'+usertext,
-        success: function(data) {                                                
-            console.log(data)
-        }
-    });
-}
-runPy()
+
+    function runPy(userInput) {
+        $.ajax({
+        data : userInput,
+            url:'http://localhost:5000/success/'+userInput,
+            success: function(data) {
+                for(var x in data.Results[0].BBlistings){
+                    let copy = $(".template").clone()
+    		    copy.removeClass("template")
+    		    copy.removeClass("d-none")
+    		    copy.find(".card-title").text(data.Results[0].BBlistings[x].name)
+                    copy.find(".card-img").attr("src",data.Results[0].BBlistings[x].pic)
+                    copy.find(".item-price").text(data.Results[0].BBlistings[x].price)
+                    copy.find(".card-website").text(data.Results[0].BBlistings[x].store)
+                    copy.find(".card-website").attr("href",data.Results[0].BBlistings[x].redirectURL)
+                
+    		    $(".cardHolder").append(copy)
+                    console.log("New card made...")
+	        }
+                                                
+                console.log(data)
+            }
+        });
+    }
+    
+    runPy(usertext);
+
+     $('#searchClick-results').click(function(){
+         let usertext = $("#userboxResults").val();
+         console.log(usertext);
+         window.location.href = 'results.html' + '#' + usertext;
+         location.reload();
+         runPy(usertext);
+     })
+});
+
+//Walmart
+$(function(){
+    var usertext = window.location.hash.substring(1)
+
+    function runPy(userInput) {
+        $.ajax({
+        data : userInput,
+            url:'http://localhost:5000/success/'+userInput,
+            success: function(data) {
+                for(var x in data.Results[1].WMlistings){
+                    let copy = $(".template").clone()
+    		    copy.removeClass("template")
+    		    copy.removeClass("d-none")
+    		    copy.find(".card-title").text(data.Results[1].WMlistings[x].name)
+                    copy.find(".card-img").attr("src",data.Results[1].WMlistings[x].pic)
+                    copy.find(".item-price").text(data.Results[1].WMlistings[x].price)
+                    copy.find(".card-website").text(data.Results[1].WMlistings[x].store)
+                    copy.find(".card-website").attr("href",data.Results[1].WMlistings[x].redirectURL)
+                
+    		    $(".cardHolder").append(copy)
+                    console.log("New card made...")
+	        }                                                
+                console.log(data)
+            }
+        });
+    }
+    
+    runPy(usertext);
 });
