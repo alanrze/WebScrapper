67fce01d662cb3cffdce230831b4af5908d14f89
mflores0572 <61068473+mflores0572@users.noreply.github.com>
2021-04-06 01:24:34 -0500

    Update server.py
    Now accepts walmart results

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/success/<name>')
 def success(name):
+   finJ = {}
+   finJ['Results']=[]
    import bestbuyServer
-   """import WalmartServer
-   temp = 'I THINK IT WORKS %s' % name
-   BB = str(bestbuyServer.test(name))
-   WM = str(WalmartServer.test(name))
-   return(BB+'\n'+WM)"""
-   return bestbuyServer.test(name)
+   import WalmartServer
+   #temp = 'I THINK IT WORKS %s' % name
+   BB = bestbuyServer.test(name)
+   WM = WalmartServer.test(name)
+   #dic['WM'] = WM
+   #dic['BB'] = BB
+   finJ['Results'].append(BB)
+   finJ['Results'].append(WM)
+   return finJ
 
 if __name__ == '__main__':
    app.run(debug = True)
