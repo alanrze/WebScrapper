e4d4b36c4fd2ebc0542d22aba9f5df585ac25adf
mflores0572 <61068473+mflores0572@users.noreply.github.com>
2021-04-21 21:49:18 -0500

    Update WalmartServer.py
    works properly, grabs stock info, and checks for price ranges now.

--- WalmartServer.py
+++ WalmartServer.py
@@ -6,49 +6,66 @@ import json
 
 
 def test(query):
-    # Connect to duckduckgo
-    URL = "https://www.walmart.com/search/?query=" + query
+    URL = "https://www.walmart.com/search/?grid=true&query=" + query
     browser = mechanicalsoup.StatefulBrowser(user_agent='MechanicalSoup')
     browser.open(URL)
 
     page = browser.get_current_page()
-    resultsPrice = page.find_all('span', attrs={'class': 'price display-inline-block arrange-fit price price-main'})
-    resultsName = page.find_all('a', attrs={'class': 'product-title-link line-clamp line-clamp-2 truncate-title'})
-    resultsPics = page.find_all('img', attrs={'data-pnodetype': 'item-pimg'})
-
-    #element1 = page.select('div.priceView-customer-price > span:first-child')
-
-    #print(element1, resultsName, resultsPics)
     itrt = 0
     finJ = {}
     finJ['WMlistings']=[]
-    #data = finJ['BBlistings']
     
-    #print(resultsPrice, resultsName, resultsPics)
+    i=10
+    results = page.find_all('li', attrs = {'class':'Grid-col'})
+    print("BELOW")
+    print("FIND", len(results))
     
-    for (x,y,z) in zip(resultsPics, resultsName, resultsPrice):
-        #linkStrt=resultsName[itrt].find('a')
-        linkFin="https://www.walmart.com/"+resultsName[itrt]['href']
-        priceStr = str(resultsPrice[itrt].text)
-        strLenHalf = int((len(priceStr)/2))
-        pictureLink = str(x['src'])
+    for x in results:
         dic = {}
-        dic['name']=str(resultsName[itrt].text)
-        dic['price']=priceStr[0:strLenHalf]
-        dic['pic']=resultsPics[itrt]['src']
-        dic['redirectURL']=str(linkFin)
-        dic['store']="Walmart"
+        resultsRange = []
+        resultsPrice = x.find('span' , attrs={'class' : 'price-main-block'})
+        resultsName = x.find('a', attrs={'class': 'product-title-link line-clamp line-clamp-2 truncate-title'})
+        resultsPics = x.find('img', attrs={'data-pnodetype': 'item-pimg'})
+        resultsATC = x.find('button', attrs={'class': 'button stepper-toggle__bttn button--ghost'})
+        resultsCOB = x.find('a', attrs={'class': 'button--choose-options'})
+        linkStrt=x.find('a')
+        linkFin="https://www.walmart.com/"+linkStrt['href']
+        if(resultsPrice is None):
+            continue
         
-        finJ['WMlistings'].append(dic)
-        #print(x,y,z)
-        itrt = itrt+1
-        if(itrt>=5):
-           break
+        print(resultsPrice.text)
+        tempPrice = str(resultsPrice.text).split('$')
+        print(tempPrice)
+        if(len(tempPrice) == 5):
+            dic['priceRange'] = True
+            dic['price'] = float(tempPrice[1])
+            dic['priceHigh'] = float(tempPrice[4])
+        else:
+            dic['priceRange'] = False
+            dic['price'] = float(tempPrice[1])
+        
+        if(resultsATC is None and resultsCOB is None):
+            dic['stock']="OOS"
+            #print("OOS")
+        else:
+            dic['stock']="In Stock"
+            #print("IS")
+        
+        dic['combo']=1
+        dic['store']="Walmart"
+        dic['name']=str(resultsName.text)
+        dic['pic']=resultsPics['src']
+        dic['redirectURL']=str(linkFin)
+        
+        print(dic)
+        #if(itrt>=5):
+        #   break
+    
 
     print()
 
-    print(finJ)
+    print("FindJ", finJ)
     return finJ
     
     
-#test("tv")
+test("tv")
