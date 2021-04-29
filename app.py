a7bad5e317f042ee876469e9a3b03930605f8d05
mflores0572 <61068473+mflores0572@users.noreply.github.com>
2021-04-28 22:00:03 -0500

    Update app.py
    added threading for faster runtime

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
 from flask import Flask, redirect, url_for, request
 from flask_cors import CORS
 import json
+import threading
 
 app = Flask(__name__)
 CORS(app)
+stores = 2
+finJ={}
 
 @app.route('/success/<name>')
 def success(name):
-   finJ={}
-   import bestbuyServer
-   import WalmartServer
-   BB = bestbuyServer.test(name)
-   WM = WalmartServer.test(name)
-   finJ['Results']=BB['BBlistings']
-   finJ['Results'].extend(WM['WMlistings'])
+   threads= []
    print("returning......",finJ)
+   for i in range(stores):
+      t = threading.Thread(target=switcher(name, i))
+      t.daemon = True
+      threads.append(t)
+   for i in range(stores):
+      threads[i].start()
+   for i in range(stores):
+      threads[i].join()
+      
+   print('HI')
    return finJ
+finJ['Results']={}
+def switcher(name, x):
+   #finJ['Results'] = {'id':2, 'name': 'temp'}
+   if(x==0):
+      import bestbuyServer
+      BB = bestbuyServer.test(name)
+      print('HI')
+      finJ['Results']=BB['BBlistings']
+   elif(x==1):
+      import WalmartServer
+      WM = WalmartServer.test(name)
+      print('HI1')
+      finJ['Results'].extend(WM['WMlistings'])
+   print(x)
 
 if __name__ == '__main__':
    app.run(debug = True)
+   
